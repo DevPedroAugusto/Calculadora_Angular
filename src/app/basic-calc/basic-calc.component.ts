dbc0254e667e8ea5e4f44454536b157ff221bf8b
Pedro Augusto <pedro.augusto@unisan.local>
2023-11-23 21:07:37 -0300

    Update function

--- src/app/basic-calc/basic-calc.component.ts
+++ src/app/basic-calc/basic-calc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChoicesComponent } from './choices/choices.component';
 import { OptionsComponent } from './options/options.component';
@@ -8,8 +8,24 @@ import { OptionsComponent } from './options/options.component';
   standalone: true,
   imports: [CommonModule, ChoicesComponent, OptionsComponent],
   templateUrl: './basic-calc.component.html',
-  styleUrl: './basic-calc.component.css'
+  styleUrls: ['./basic-calc.component.css']
 })
 export class BasicCalcComponent {
 
+  constructor() {
+    const closeCalcBtn = document.querySelector('#close-calc_btn');
+  }
+  
+  ngOnInit() {
+    // declarando variaveis basicas e reutilizaveis
+  }
+  
+  moveCalc(){
+    const closeCalcBtn = document.querySelector('#close-calc_btn');
+    console.log("cliclou")
+  }
+
+  stopMoveCalc(){
+    console.log("mouseSaiu")
+  }
 }
